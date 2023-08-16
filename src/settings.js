cd4a99e1aea8967c99a31a456e1a60df688d52c1
张惠琼 <zhanghuiqiong@egoonet.com>
2023-08-16 18:21:36 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+/**
+   * @type {项目名称} title
+   * @description Whether fix the header
+   */
+  title: '人力资源管理',
+
+  /**
+   * @type {头部固定} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {侧边栏的logo} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
